00e2e3f41dea66e2df4351d57bc20a041c267943
Serg <esv2@ulstu.ru>
2023-11-02 15:20:48 +0400

    12

---
+++ main.py
@@ -0,0 +1,14 @@
+# https://yandex.ru/video/preview/13198009012106879976
+
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return  Button(text="Hi")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    MyApp().run()
+
